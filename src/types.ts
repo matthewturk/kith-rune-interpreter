ff71740cab1f56ce953f7da83e80a0d4124f7df0
Matthew Turk <matthewturk@gmail.com>
2025-06-07 13:58:24 -0500

    Starting to add options for creating the rune actions

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,18 @@
-export interface GreetOptions {
-	logger?: (message: string) => void;
-	message: string;
-	times?: number;
+export interface IRunePressCondition {
+	variable: string;
+	condition: "equals" | "not_equals" | "greater_than" | "less_than";
+	value: string | number | boolean;
 }
+
+export type ActionType =
+	| "set_variable"
+	| "increment_variable"
+	| "decrement_variable"
+	| "output_message";
+export interface ISetVariableAction {
+	actionType: "set_variable";
+	variable: string;
+	value: string | number | boolean;
+}
+
+export type IRunePressAction = ISetVariableAction;
